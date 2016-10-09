9484fcadddec75ad7a42ef1c7201c898a55db7b7
Gavin Engel <gavin@engel.com>
2016-10-09 17:46:03 -0400

    .

--- Bucketlist/dataProviders/bucketlist.js
+++ Bucketlist/dataProviders/bucketlist.js
@@ -44,7 +44,7 @@
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 var scheme = (navigator.platform.indexOf('Win32') == -1) ? 'http' : 'https';
 if (scheme != 'https') {
-    app.data.bucketlist.setup.scheme = scheme;
+    //app.data.bucketlist.setup.scheme = scheme;
     //console.log('modified protocol for: ' + navigator.platform);
 }
 // END_CUSTOM_CODE_bucketlist
