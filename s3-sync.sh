048dbc066cb2b78c9e27b7f45e3c8345959b9399
Gavin Engel <gavin@engel.com>
2016-10-09 18:12:26 -0400

    .

--- s3-sync.sh
+++ s3-sync.sh
@@ -1,4 +1,5 @@
+#!/bin/sh
 aws s3 sync --delete ./Bucketlist/ s3://bucketlistapp --exclude=.* --acl public-read;
 
-Endpoint: bucketlistapp.s3-website-us-east-1.amazonaws.com
+echo "Endpoint: bucketlistapp.s3-website-us-east-1.amazonaws.com";
 
